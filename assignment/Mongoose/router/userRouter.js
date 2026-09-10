350a4f79562f27f740c1cfb5d4157f5e6aa6e616
krish shinde <krishshinde@krishs-MacBook-Air.local>
2026-09-10 15:52:25 +0530

    assignment updated

--- assignment/Mongoose/router/userRouter.js
+++ assignment/Mongoose/router/userRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 const User = require('../model/userModel');
 const router = express.Router();
+
+// PART 1 ROUTES 
 
 router.post('/api/users', async (req, res) => {
     try {
@@ -21,6 +24,51 @@ router.get('/api/users', async (req, res) => {
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ message: "Error retrieving users", error: error.message });
+    }
+});
+
+// PART 2 ROUTES 
+router.patch('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid MongoDB ID" });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { 
+            new: true, 
+            runValidators: true 
+        });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "User updated successfully", user: updatedUser });
+
+    } catch (error) {
+        res.status(500).json({ message: "Database error", error: error.message });
+    }
+});
+
+router.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params; 
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid MongoDB ID" });
+        }
+
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "User deleted successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Database error", error: error.message });
     }
 });
 
